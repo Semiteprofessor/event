404456920cbefbb09abc51b78198557c52bea056
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-26 15:42:35 -0700

    Commit

---
+++ src/main/java/com/event/events/model/User.java
@@ -0,0 +1,57 @@
+package com.event.events.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String bio;
+
+    private Role role = Role.GUEST;
+
+    private boolean isEmailVerified = false;
+
+    private String refreshToken;
+    private Date refreshTokenExpires;
+
+    private String resetToken;
+    private Date resetTokenExpires;
+
+    private boolean isAdmin = false;
+    private String profileImage = "";
+
+    private UserType userType = UserType.FREE;
+
+    private boolean isOnboarded = false;
+
+    private VendorOnboarding onboarding;
+
+    private Notifications notifications = new Notifications();
+
+    private Provider provider = Provider.UNKNOWN;
+    private String providerId;
+
+    private String avatar;
+
+    private boolean isBanned = false;
+    private String banReason = "";
+    private Date bannedUntil;
+
+    private Date createdAt;
+    private Date updatedAt;
+}
