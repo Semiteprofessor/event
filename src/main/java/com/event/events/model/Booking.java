b4703d50d115d1c0837d05aefa27bd4c0e73a8a0
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-26 16:22:07 -0700

    Commit

---
+++ src/main/java/com/event/events/model/Booking.java
@@ -0,0 +1,44 @@
+package com.event.events.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "bookings")
+public class Booking {
+
+    @Id
+    private String id;
+
+    private String event; // ObjectId → String
+    private Date eventDate;
+
+    private String userEmail;
+    private String posterEmail;
+
+    private List<BookingTicket> tickets;
+
+    private Integer ticketsCount;
+
+    private Double totalAmount;
+
+    private List<String> ticketsGenerated;
+
+    private BookingStatus status = BookingStatus.PAID;
+
+    private String vendor; // ObjectId → String
+
+    private VendorStatus vendorStatus = VendorStatus.PENDING;
+
+    private List<Object> registeredForm;
+
+    private Date createdAt;
+    private Date updatedAt;
+}
