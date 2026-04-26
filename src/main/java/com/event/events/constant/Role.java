81fffb23499db770bd17148db48aa8e5f51f147b
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-26 15:46:33 -0700

    Commit

---
+++ src/main/java/com/event/events/constant/Role.java
@@ -0,0 +1,5 @@
+package com.event.events.constant;
+
+public enum Role {
+    SUPER_ADMIN, ADMIN, EDITOR, VENDOR, GUEST
+}
--- src/main/java/com/event/events/model/User.java
+++ src/main/java/com/event/events/model/User.java
@@ -1,5 +1,6 @@
 package com.event.events.model;
 
+import com.event.events.constant.Role;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
