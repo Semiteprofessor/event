4a41a4a1a581ca8778de51aeccfb140ad88ea74e
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-27 08:51:52 -0700

    Commit

--- src/main/java/com/event/events/model/WishlistSupport.java
+++ src/main/java/com/event/events/model/WishlistSupport.java
@@ -4,13 +4,32 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Date;
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Document(collection = "wishlist_support")
 public class WishlistSupport {
 
     @Id
     private String id;
 
+    @NotBlank
     private String wishlist;
-    private Double amount;
+
+    @NotBlank
+    private String supporter;
+
+    @Min(1)
+    private double amount;
+
+    private String supporterName;
+
+    private Date createdAt;
+    private Date updatedAt;
 }
