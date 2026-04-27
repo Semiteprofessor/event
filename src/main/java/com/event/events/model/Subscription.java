03363ea78804fb0df29e023e7cfcab81ff32d139
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-27 09:02:07 -0700

    Commit

---
+++ src/main/java/com/event/events/model/Subscription.java
@@ -0,0 +1,29 @@
+package com.event.events.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "subscriptions")
+public class Subscription {
+
+    @Id
+    private String id;
+
+    private String userId;
+    private String planId;
+
+    private SubscriptionStatus status;
+
+    private Date startDate;
+    private Date endDate;
+
+    private String paymentProvider; // stripe / paystack
+    private String paymentReference;
+}
