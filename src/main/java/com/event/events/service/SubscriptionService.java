5488243784920d9ec49d70c59183c8089c22f4a0
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-27 09:23:53 -0700

    Commit

---
+++ src/main/java/com/event/events/service/SubscriptionService.java
@@ -0,0 +1,13 @@
+package com.event.events.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+
+    public void activateSubscription(String userId, String planId) {
+        // create subscription
+        // set start/end date
+        // update user plan
+    }
+}
