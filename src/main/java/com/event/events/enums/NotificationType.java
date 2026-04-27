2a1ee6e7f311e257aa999270c76e539f8d28ffaf
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-27 08:37:44 -0700

    Commit

---
+++ src/main/java/com/event/events/enums/NotificationType.java
@@ -0,0 +1,12 @@
+package com.event.events.enums;
+
+public enum NotificationType {
+    BOOKING,
+    BOOKING_REQUEST,
+    INSTALLMENT,
+    EVENT_UPDATE,
+    PAYMENT,
+    EVENT,
+    CHAT,
+    SYSTEM
+}
