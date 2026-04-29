43af5b805e4e271660e415b5e29cb91dccdc3399
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-29 14:06:30 -0700

    Commit

---
+++ src/main/java/com/event/events/util/PasswordUtil.java
@@ -0,0 +1,12 @@
+package com.event.events.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtil {
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public static boolean matches(String raw, String hashed) {
+        return encoder.matches(raw, hashed);
+    }
+}
