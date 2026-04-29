0b72fd4f1ba84b7f4bee7768c3f5ee9de190ae13
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-29 14:03:40 -0700

    Commit

---
+++ src/main/java/com/event/events/service/TokenService.java
@@ -0,0 +1,39 @@
+package com.event.events.service;
+
+import com.event.events.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    public String refreshToken(String token) {
+
+        if (token == null) {
+            throw new RuntimeException("Refresh token missing");
+        }
+
+        Claims payload;
+
+        try {
+            payload = jwtService.verifyRefreshToken(token);
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid or expired refresh token");
+        }
+
+        String userId = payload.getSubject();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!token.equals(user.getRefreshToken())) {
+            throw new RuntimeException("Refresh token invalidated");
+        }
+
+        return jwtService.generateToken(user);
+    }
+}
