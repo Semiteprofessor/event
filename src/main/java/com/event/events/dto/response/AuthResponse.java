9ad6ed69e0f60bba7ce631243b3667a2efd3ee1d
Taiwo Olapade <semiteprofessor@gmail.com>
2026-05-01 08:56:50 -0700

    Commit

--- src/main/java/com/event/events/dto/response/AuthResponse.java
+++ src/main/java/com/event/events/dto/response/AuthResponse.java
@@ -1,16 +1,38 @@
 package com.event.events.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AuthResponse {
-    private int status;
-    private Object body;
-    private String token;
 
-    public AuthResponse(int value, ApiResponse success, String accessToken, String refreshToken, String role) {
+    private int status;
+    private ApiResponse body;
+
+    private String accessToken;
+    private String refreshToken;
+    private String role;
+
+    public AuthResponse(int status, ApiResponse body) {
+        this.status = status;
+        this.body = body;
     }
 
-    public AuthResponse(int value, ApiResponse success, Object accessToken) {
+    public AuthResponse(int status, ApiResponse body, String accessToken) {
+        this.status = status;
+        this.body = body;
+        this.accessToken = accessToken;
+    }
+
+    public AuthResponse(int status, ApiResponse body,
+                        String accessToken,
+                        String refreshToken,
+                        String role) {
+        this.status = status;
+        this.body = body;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.role = role;
     }
 }
