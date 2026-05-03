62177130f7039d0ecccb52cbe7668d6519e0889f
Taiwo Olapade <semiteprofessor@gmail.com>
2026-05-03 13:44:47 -0700

    Commit

--- src/main/java/com/event/events/repository/EventRepository.java
+++ src/main/java/com/event/events/repository/EventRepository.java
@@ -1,11 +1,20 @@
 package com.event.events.repository;
 
 import com.event.events.model.Event;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
-public interface EventRepository extends JpaRepository<Event, String> {
+public interface EventRepository
+        extends JpaRepository<Event, String> {
 
-    Optional<Event> findBySlug(String slug);
+    Event findBySlug(String slug);
+
+    List<Event> findByUserId(String userId);
+
+    List<Event> findByNameContainingIgnoreCase(
+            String search,
+            Pageable pageable
+    );
 }
