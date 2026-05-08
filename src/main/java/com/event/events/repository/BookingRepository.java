1459c66f2df1f3e85ebf4f5cf238a325dd00a64c
Taiwo Olapade <semiteprofessor@gmail.com>
2026-05-08 07:34:23 -0700

    Commit

---
+++ src/main/java/com/event/events/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package com.event.events.repository;
+
+import com.event.events.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository
+        extends JpaRepository<Booking, String> {
+
+    List<Booking> findByUserEmail(String userEmail);
+}
