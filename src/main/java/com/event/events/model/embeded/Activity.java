5428f54e14a2c4f0961fabbafec62add7974d15d
Taiwo Olapade <semiteprofessor@gmail.com>
2026-05-08 03:24:57 -0700

    Commit

--- src/main/java/com/event/events/model/embeded/Activity.java
+++ src/main/java/com/event/events/model/embeded/Activity.java
@@ -1,13 +1,23 @@
 package com.event.events.model.embeded;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
 import lombok.Data;
+
 import java.util.List;
 
 @Data
+@Embeddable
 public class Activity {
+
     private String name;
+
     private String activityStartTime;
+
     private String activityStopTime;
+
     private String speaker;
+
+    @ElementCollection
     private List<String> panelists;
 }
--- src/main/java/com/event/events/model/embeded/InstallmentConfig.java
+++ src/main/java/com/event/events/model/embeded/InstallmentConfig.java
@@ -1,13 +1,21 @@
 package com.event.events.model.embeded;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
 import lombok.Data;
-import java.util.Date;
+
+import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.List;
 
 @Data
+@Embeddable
 public class InstallmentConfig {
 
     private Integer numberOfInstallments = 1;
-    private Double minPerInstallment = 0.0;
-    private List<Date> dueDates;
+
+    private BigDecimal minPerInstallment = BigDecimal.ZERO;
+
+    @ElementCollection
+    private List<Instant> dueDates;
 }
