522b20a65a3a03bed8e91750ff85329918269492
Taiwo Olapade <semiteprofessor@gmail.com>
2026-05-08 07:43:25 -0700

    Commit

---
+++ src/main/java/com/event/events/dto/request/BookingRequest.java
@@ -0,0 +1,25 @@
+package com.event.events.dto.request;
+
+import com.event.events.model.embeded.TicketType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BookingRequest {
+
+    @Email
+    @NotBlank
+    private String userEmail;
+
+    @NotBlank
+    private String event;
+
+    @Valid
+    @NotEmpty
+    private List<TicketType> tickets;
+}
--- src/main/java/com/event/events/repository/TicketRepository.java
+++ src/main/java/com/event/events/repository/TicketRepository.java
@@ -1,4 +1,21 @@
 package com.event.events.repository;
 
-public class TicketRepository {
+import com.event.events.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TicketRepository
+        extends JpaRepository<Ticket, String> {
+
+    List<Ticket> findByUserEmail(String userEmail);
+
+    Optional<Ticket> findByQrSlug(String qrSlug);
+
+    List<Ticket> findByBooking(String booking);
+
+    List<Ticket> findByEvent(String event);
+
+    List<Ticket> findByCheckedIn(boolean checkedIn);
 }
